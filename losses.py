72a42d5430a1bd573e445126449e763c4f2caf43
Ethan Ng <wlx5502@outlook.com>
2021-11-25 23:14:54 +0800

    refactor: refactor the way to select anchors, positives and negatives
    Owing to special data batch structure set before,
    split the data in a lazy way.

--- losses.py
+++ losses.py
@@ -106,19 +106,23 @@ class NPairLoss(nn.Module):
         :return: A tuple of n_pairs (n, 2)
                         and n_negatives (n, n-1)
         """
-        labels = labels.cpu().data.numpy()
-        n_pairs = []
+        # labels = labels.cpu().data.numpy()
+        # n_pairs = []
 
-        for label in set(labels):
-            label_mask = (labels == label)
-            label_indices = np.where(label_mask)[0]
-            if len(label_indices) < 2:
-                assert(False, "Only face or cartoon is provided.")
-            # anchor, positive = label_indices # np.random.choice(label_indices, 2, replace=False)
-            n_pairs.append(label_indices)
+        # for label in set(labels):
+        #     label_mask = (labels == label)
+        #     label_indices = np.where(label_mask)[0]
+        #     if len(label_indices) < 2:
+        #         assert(False, "Only face or cartoon is provided.")
+        #     # anchor, positive = label_indices # np.random.choice(label_indices, 2, replace=False)
+        #     n_pairs.append(label_indices)
 
-        n_pairs = np.array(n_pairs)
-
+        # n_pairs = np.array(n_pairs)
+        
+        n_pairs = np.arange(2*labels.size(0))\
+                    .reshape((2, -1))\
+                    .transpose(1, 0)
+        
         n_negatives = []
         for i in range(len(n_pairs)):
             negative = np.concatenate([n_pairs[:i, 1], n_pairs[i+1:, 1]])
